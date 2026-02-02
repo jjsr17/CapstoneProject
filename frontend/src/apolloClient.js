72eee83ecdbd78c730297f67dd8c5fbd662075a6
Jonathan Sewell <jsewellro@gmail.com>
2026-02-01 20:48:01 -0400

    ApolloClient update

--- frontend/src/apolloClient.js
+++ frontend/src/apolloClient.js
@@ -1,3 +1,10 @@
+import { ApolloClient, InMemoryCache, HttpLink, from } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+const httpLink = new HttpLink({
+  uri: import.meta.env.VITE_GRAPHQL_URL, // or hardcode your URL
+});
+
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem("msAccessToken");
   const useMs = localStorage.getItem("useMsSso") === "true";
@@ -5,7 +12,12 @@ const authLink = setContext((_, { headers }) => {
   return {
     headers: {
       ...headers,
-      Authorization: useMs && token ? `Bearer ${token}` : "",
+      ...(useMs && token ? { Authorization: `Bearer ${token}` } : {}),
     },
   };
+});
+
+export const client = new ApolloClient({
+  link: from([authLink, httpLink]),
+  cache: new InMemoryCache(),
 });
